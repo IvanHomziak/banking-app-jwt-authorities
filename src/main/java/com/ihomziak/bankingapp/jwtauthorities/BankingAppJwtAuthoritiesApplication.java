f36a538c13459d7d08424124e66f2f02fd23064f
ivanhomziak <ivan.homziak@gmail.com>
2025-01-19 21:24:07 +0100

    Initial commit

---
+++ src/main/java/com/ihomziak/bankingapp/jwtauthorities/BankingAppJwtAuthoritiesApplication.java
@@ -0,0 +1,13 @@
+package com.ihomziak.bankingapp.jwtauthorities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppJwtAuthoritiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingAppJwtAuthoritiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ihomziak/bankingapp/jwtauthorities/BankingAppJwtAuthoritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihomziak.bankingapp.jwtauthorities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppJwtAuthoritiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
